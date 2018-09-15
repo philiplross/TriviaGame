22462adf7815d90342b6c20069004c56e1842279
Philip Ross <philiplross@gmail.com>
2018-09-15 09:37:41 -0700

    v1.8
    v1.8

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,15 @@
 alert("Wlecome to phil's Trivia game, Just remeber you have 120 seconds to finish the game before it ends. Goodluck!");
 
-var MyFunction = function(){
-
-    var counter = 10;
-    var timer = null;
- 
-    function countdown(){
-        if (counter == 120) {
-            $(".time_box").html(counter);
-        }
-        if (counter <= 0) {
-            stopInterval();
-        }
-        else {
-            counter--;
-            $(".time_box").html(counter);
-        }
-     }
-     function reset() {
-        clearInterval(timer);
-        counter=0;
-     }
-     function startInterval() {
-        $(".time_box").html(counter);
-        timer= setInterval(countdown, 1000);
-     }
-     function stopInterval() {
-        clearInterval(timer);
-     }
- 
-     return {
-       startInterval: startInterval
-     }
-     $("#countdown").attr(MyFunction)
- }();
-
-
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var grand = 0;
 
-$("#q1a").click(function(){
-    correctAnswers + 1;
-    return grand + correctAnswers;
-});
+$("#q1").on('click',function(){
+    correctAnswers= correctAnswers + 1;
+    console.log(correctAnswers, "correct value")
+     return grand + correctAnswers;
+})
 
 $("#q1b").click(function(){
 incorrectAnswers + 1
@@ -92,7 +57,40 @@ $("#q5b").click(function(){
     incorrectAnswers + 1;
 });
 
+var MyFunction = function(){
 
+    var counter = 10;
+    var timer = null;
+ 
+    function countdown(){
+        if (counter == 120) {
+            $(".time_box").html(counter);
+        }
+        if (counter <= 0) {
+            stopInterval();
+        }
+        else {
+            counter--;
+            $(".time_box").html(counter);
+        }
+     }
+     function reset() {
+        clearInterval(timer);
+        counter=0;
+     }
+     function startInterval() {
+        $(".time_box").html(counter);
+        timer= setInterval(countdown, 1000);
+     }
+     function stopInterval() {
+        clearInterval(timer);
+     }
+ 
+     return {
+       startInterval: startInterval
+     }
+     $("#countdown").attr(MyFunction)
+ }();
 
 
 console.log(correctAnswers);
