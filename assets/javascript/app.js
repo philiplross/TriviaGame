4e7cd20846eae6d1d7ce3a70786f29932d5b39e1
Philip Ross <philiplross@gmail.com>
2018-09-14 19:42:00 -0700

    v1.3
    bootstrap done and more jquerry

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,7 @@
 alert("Wlecome to phil's Trivia game, Just remeber you have 120 seconds to finish the game before it ends. Goodluck!");
 
-var questionOne {
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 
-};
-
-var questionTwo{
-
-};
-
-var questionThree{
-
-};
-
-var questionFour{
-
-};
+$("#q1a").click(correctAnswers + 1);
+console.log(this);
