bd91f94212ab38d7393c3418fc02ebe82a5b8bb5
Philip Ross <philiplross@gmail.com>
2018-09-14 20:15:06 -0700

    v1.6
    jquery issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,53 @@ var incorrectAnswers = 0;
 
 $("#q1a").click(function(){
     correctAnswers + 1;
-   $(correctAnswers).html("your current score is" +correctAnswer)});
-console.log(correctAnswers);
-//$("#q1b").click(function(){
-    
+  
+});
 
+$("#q1b").click(function(){
+incorrectAnswers + 1
+});
+
+$("#q2a").click(function(){
+    incorrectAnswers + 1;
+  
+});
+
+$("#q2b").click(function(){
+    correctAnswers + 1;
+});
+
+
+$("#q3a").click(function(){
+    incorrectAnswers + 1;
+  
+});
+
+$("#q3b").click(function(){
+    correctAnswers + 1;
+});
+
+
+$("#q4a").click(function(){
+    incorrectAnswers + 1;
+  
+});
+
+$("#q4b").click(function(){
+    correctAnswers + 1;
+});
+
+$("#q5a").click(function(){
+    correctAnswers + 1;
+  
+});
+
+$("#q5b").click(function(){
+    incorrectAnswers + 1;
+});
+
+
+
+
+console.log(correctAnswers);
+console.log(incorrectAnswers);
