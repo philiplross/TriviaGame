a95bac9d7c928ca03cd610d2747f551d1eda4825
Philip Ross <philiplross@gmail.com>
2018-09-14 19:09:28 -0700

    v1.3
    v1.3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,17 @@
-alert("Wlecome to phil's Trivia game")
+alert("Wlecome to phil's Trivia game, Just remeber you have 120 seconds to finish the game before it ends. Goodluck!");
+
+var questionOne {
+
+};
+
+var questionTwo{
+
+};
+
+var questionThree{
+
+};
+
+var questionFour{
+
+};
